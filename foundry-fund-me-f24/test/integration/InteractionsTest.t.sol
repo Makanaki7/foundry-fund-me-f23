a007fdb9ee0c19ce60b5c73127794a3a9f26dc80
comrade <tamunosakimillerwest@gmail.com>
2025-02-20 11:08:44 +0300

    modules

---
+++ foundry-fund-me-f24/test/integration/InteractionsTest.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "../../lib/forge-std/src/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
+
+contract InteractionsTest is Test {
+    FundMe private fundMe;
+
+    address private constant USER = address(0x123); // Example user address
+    uint256 private constant SEND_VALUE = 0.1 ether; // 0.1 ether
+    uint256 private constant STARTING_BALANCE = 10 ether;
+    uint256 private constant GAS_PRICE = 1;
+
+    function setUp() external {
+        DeployFundMe deploy = new DeployFundMe();
+        fundMe = deploy.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+
+    function testUserCanFundInteractions() public {
+        FundFundMe fundFundMe = new FundFundMe();
+        fundFundMe.fundFundMe(address(fundMe));
+
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address(fundMe));
+
+        assertEq(address(fundMe).balance, 0);
+    }
+}
